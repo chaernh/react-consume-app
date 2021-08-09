391d6fe2286d13aab5925add920646d0a2dd8b12
CHAER <chaerfansyah.hidayah@gmail.com>
2021-08-09 12:02:05 +0700

    adjust styling for login component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header'
 import Categories from './pages/Categories'
 import CategoryDetails from './pages/CategoryDetails'
+import Login from './pages/Login';
 import Footer from './components/Footer'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
                 </div>
                 <div id="body-content" className="section-mb">
                     <Switch>
+                        <Route path="/login" component={Login}></Route>
                         <Route exact path="/" component={Categories}></Route>
                         <Route path="/:id" component={CategoryDetails}></Route>
                     </Switch>
